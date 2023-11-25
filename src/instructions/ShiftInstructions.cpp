5a17fa1bde8d0b648d1a0288a7b3e1758f1107b0
Keony <keonyvictor@gmail.com>
2023-11-25 20:05:33 -0300

    shift fix

--- src/instructions/ShiftInstructions.cpp
+++ src/instructions/ShiftInstructions.cpp
@@ -3,13 +3,18 @@
 
 void ShiftLeft::execute(unsigned int& PC, Integer &AC, Integer* memory, Integer num) {
     PC++;
-    AC = Integer(AC.toString().erase(1) + "0");
+    std::string t = AC.toString();
+    t.erase(1, 1);
+    AC = Integer(t + "0");
 }
 
 void ShiftRight::execute(unsigned int& PC, Integer &AC, Integer* memory, Integer num) {
     PC++;
     std::string t = AC.toString();
     char signal = t[0];
-    t.erase(0);
-    AC = Integer(signal + "0" + t.erase(3));
+    t.erase(0,1);
+    t.erase(3,1);
+    t = '0' + t;
+    t = signal + t;
+    AC = Integer(t);
 }
